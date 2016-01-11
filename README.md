fcd8b1f3bbb86ad7128dc67e1772ece97b164b4b
joshua-anderson <j@joshua-anderson.com>
2016-01-10 21:39:33 -0800

    Switch to bintay

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 This is a hourly travis-ci ([![Build Status](https://travis-ci.org/Joshua-Anderson/atom-uploader.svg?branch=master)](https://travis-ci.org/Joshua-Anderson/atom-uploader)) job that checks if there is a new version of atom available.
 
-If there is a new version it uploads it to package cloud (vivid and utopic distributions).
+If there is a new version it uploads it to a bintray debian repositories.
 
-To setup the apt repository, go to https://packagecloud.io/joshua-anderson/atom or follow these instructions:
+To setup the apt repository, run:
 
 ``` shell
-$ curl -s https://packagecloud.io/install/repositories/joshua-anderson/atom/script.deb.sh | sudo bash
+$ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 99246077F3B16285
+$ sudo apt-add-repository 'deb https://dl.bintray.com/joshua-anderson/atom wheezy main'
+$ sudo apt-get update
 $ sudo apt-get install atom
 ```
--- atom-uploader.rb
+++ atom-uploader.rb
@@ -20,11 +20,11 @@ atom_version = body["name"]
 
 puts "Atom Version: " + atom_version
 
-url = URI.parse('https://packagecloud.io')
+url = URI.parse('https://api.bintray.com/')
 http = Net::HTTP.new(url.host, url.port)
 http.use_ssl = true
-req = Net::HTTP::Get.new("/api/v1/repos/joshua-anderson/atom/package/deb/ubuntu/trusty/atom/amd64/versions.json")
-req.basic_auth ENV['PACKAGECLOUD_TOKEN'], ''
+req = Net::HTTP::Get.new("/packages/joshua-anderson/atom/atom/versions/_latest")
+req.basic_auth 'joshua-anderson', ENV['BINTRAY_TOKEN']
 
 res = http.request(req)
 
@@ -32,19 +32,48 @@ if res.code != "200"
   puts "Error Code: " + res.code
   raise res.body
 end
+
 body = JSON.parse(res.body)
 
-uploaded_version = body[0]["version"]
+uploaded_version = body["name"]
 
-puts 'Uploaded Version: ' + uploaded_version
+puts "Uploaded Version: " + uploaded_version
 
 if atom_version == uploaded_version
   puts 'Already up to date!'
   exit
 end
 
+req = Net::HTTP::Post.new("/packages/joshua-anderson/atom/atom/versions")
+req.basic_auth 'joshua-anderson', ENV['BINTRAY_TOKEN']
+req.body = "{
+\"name\":\"#{atom_version}\",
+}"
+req.content_type = 'application/json'
+
+res = http.request(req)
+
+if res.code != "201"
+  puts "Error Code: " + res.code
+  raise res.body
+end
+
+puts "Created Version: " + atom_version
+
 raise "Error" if !system('curl -L -o $PWD/atom.deb https://atom.io/download/deb')
-raise "Error" if !system('bundle exec package_cloud push joshua-anderson/atom/ubuntu/trusty atom.deb')
-raise "Error" if !system("bundle exec package_cloud yank joshua-anderson/atom/ubuntu/trusty atom_#{uploaded_version}_amd64.deb")
-raise "Error" if !system('bundle exec package_cloud push joshua-anderson/atom/ubuntu/wily atom.deb')
-raise "Error" if !system("bundle exec package_cloud yank joshua-anderson/atom/ubuntu/wily atom_#{uploaded_version}_amd64.deb")
+
+puts "Downloaded Version: " + atom_version
+
+raise "Error" if !system("curl -T atom.deb -H \"X-Bintray-Debian-Distribution: wheezy\" -H \"X-Bintray-Debian-Component: main\" -H \"X-Bintray-Debian-Architecture: amd64\" -ujoshua-anderson:#{ENV['BINTRAY_TOKEN']} https://api.bintray.com/content/joshua-anderson/atom/atom/#{atom_version}/pool/main/a/atom_#{atom_version}_amd64.deb")
+
+req = Net::HTTP::Post.new("/content/joshua-anderson/atom/atom/#{atom_version}/publish")
+req.basic_auth 'joshua-anderson', ENV['BINTRAY_TOKEN']
+
+res = http.request(req)
+
+if res.code != "200"
+  puts "Error Code: " + res.code
+  raise res.body
+end
+
+puts "Published Version: " + atom_version
